b20db0f730b5848cc09d71b1ef5df91c9ddd5a9b
Hyunwook Woo <hwwoo@satreci.com>
2022-01-31 14:43:15 +0900

    Initial commit

---
+++ h-tapa/Constants-Environments.py
@@ -0,0 +1,21 @@
+##################
+# Constants-Environments.py
+# definitions of Earth and Astronomical Constants
+
+# General Physical Constants
+_G = Gravitational_Constanta = 6.67408e-11   # In general, written as G, unit : m^3 kg^-1 s^-2
+
+# Astronomical and Geophysical Constants
+# # Earth
+# # # Geometry and Motion
+_R_em = Radius_Mean_Earth = 6371008.8     # unit : m, Ref: Wikipedia (Earth Radius)
+_R_ee = Radius_Equatorial_Earth = 6378137.0   # unit : m, WGS-84
+_R_ep = Radius_Polar_Earth = 6356752    # unit : m, WGS-84
+_f_e = Flattening_Earth = 1.0 / 298.257223563  # unit : (N/A), WGS-84
+_w_e = AngularVelocity_Earth = 7.292115e-5  # unit : rad/sec, WGS-84
+
+# # # Mass and Gravity
+_M_e = Mass_Earth = 5972e24    # unit : kg
+_mu_e = mu_Earth = 3.986004418e14   # G x Mass_Earth, unit : m^3 / sec^2, WGS-84
+_J2 = J2_Earth = 17542930648
+
---
+++ h-tapa/constants.py
@@ -0,0 +1,15 @@
+##################
+# constatnts.py
+# definitions of Earth and Astronomical Constants
+
+# Astronomical and Geophysical Constants
+Gravitational_Constanta = 6.67408e-11   # unit : m^3 kg^-1 s^-2
+Mass_Earth = 5972e24    # unit : kg
+Radius_Mean_Earth = 6371008.8     # unit : m, Ref: Wikipedia (Earth Radius)
+Radius_Equatorial_Earth = 6378137.0   # unit : m, WGS-84
+Radius_Polar_Earth = 6356752    # unit : m, WGS-84
+Flattening_Earth = 1.0 / 298.257223563  # unit : (N/A), WGS-84
+Mu_GM = 398600.4418e8   # unit : m^3 / sec^2, WGS-84
+AngularVelocity_Earth = 7.292115e-5  # unit : rad/sec, WGS-84
+# J2_Earth = 17542930648
+# IGRF_Year = 2015
